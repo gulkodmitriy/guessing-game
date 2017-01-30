ce975c7d55821bd3f292f9cbb4f24b5d7ae2167e
Дмитрий Гулько < >
2017-01-30 20:55:27 +0300

    guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._number = 0;
+        this._min = 0;
+        this._max = 0;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._number = Math.ceil((this._min + this._max)/2);
+        return this._number;
     }
 
     lower() {
-
+        this._max = this._number--;
     }
 
     greater() {
-
+        this._min = this._number++;
     }
 }
 
